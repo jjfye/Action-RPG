2a91c59f397dbb8c5f641efc1343138b91155078
JjfyeDev <jye882@gmail.com>
2024-04-18 22:59:17 +0100

    Update Effect.gd

--- Effects/Effect.gd
+++ Effects/Effect.gd
@@ -1,11 +1,9 @@
-extends Node2D
-
-@onready var animatedSprite = $AnimatedSprite2D
+extends AnimatedSprite2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animatedSprite.play("Animate")
-
+	connect("animation_finished", Callable(self, "_on_animated_sprite_2d_animation_finished"))
+	play("Animate")
 
 func _on_animated_sprite_2d_animation_finished():
 	queue_free()
